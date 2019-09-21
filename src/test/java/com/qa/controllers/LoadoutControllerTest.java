4269fb4d721c6abdfcfbcd4f87b30bb47d0cb3d0
Jacob Eddison <jacobweddison@gmail.com>
2019-09-21 18:21:14 +0100

    Fixed tests to run on java 8 at home and added tests for loadout controller

--- src/test/java/com/qa/controllers/LoadoutControllerTest.java
+++ src/test/java/com/qa/controllers/LoadoutControllerTest.java
@@ -31,49 +31,71 @@ public class LoadoutControllerTest {
     @LocalServerPort
     private int port;
 
-//    @Test
-//    public void testGetAllLoadouts(){
-//        List<Loadout> loadoutList = new ArrayList<>();
-//        Loadout loadout = new Loadout();
-//        loadout.setHelmetId(2L);
-//        loadout.setArmId(3L);
-//        loadout.setChestId(4L);
-//        loadout.setLegId(5L);
-//        loadout.setMarkId(6L);
-//
-//        loadoutList.add(loadout);
-//
-//        when(repository.findAll()).thenReturn(loadoutList);
-//
-//        assertEquals(loadoutController.listAllLoadout().get(0).getArmId(),3L);
-//
-//    }
-//
-//    @Test
-//    public void testGetLoadout(){
-//        Loadout loadout = new Loadout();
-//        loadout.setHelmetId(10L);
-//
-//        when(repository.findOne(0L)).thenReturn(loadout);
-//
-//        long test = 10L;
-//
-//        assertEquals(loadoutController.getLoadout(0L).getHelmetId(), test);
-//
-//    }
+    @Test
+    public void testGetAllLoadouts(){
+        List<Loadout> loadoutList = new ArrayList<>();
+        Loadout loadout = new Loadout();
+        loadout.setHelmetId(2L);
+        loadout.setArmId(3L);
+        loadout.setChestId(4L);
+        loadout.setLegId(5L);
+        loadout.setMarkId(6L);
+
+        loadoutList.add(loadout);
+
+        when(repository.findAll()).thenReturn(loadoutList);
+
+        assertEquals(loadoutController.listAllLoadout().get(0).getArmId(),new Long(3));
+
+    }
+
+    @Test
+    public void testGetLoadout(){
+        Loadout loadout = new Loadout();
+        loadout.setHelmetId(10L);
+
+        when(repository.findOne(0L)).thenReturn(loadout);
+
+        assertEquals(loadoutController.getLoadout(0L).getHelmetId(), new Long(10));
+
+    }
 
     @Test
     public void testSetLoadout(){
+        Loadout loadout = new Loadout();
+        loadout.setLegId(7L);
+        loadout.setMarkId(8L);
 
+        when(repository.saveAndFlush(loadout)).thenReturn(loadout);
+
+        assertEquals(loadoutController.addLoadout(loadout).getLegId(),new Long(7));
     }
 
     @Test
     public void testDeleteLoadout(){
+        Loadout loadout = new Loadout();
+        loadout.setHelmetId(5L);
+        loadout.setChestId(7L);
 
+        when(repository.findOne(0L)).thenReturn(loadout);
+
+        assertEquals(loadoutController.deleteLoadout(0L).getChestId(), new Long(7));
     }
 
     @Test
     public void testUpdateLoadout(){
+        Loadout loadout1 = new Loadout();
+        loadout1.setArmId(4L);
+        loadout1.setLegId(6L);
+        Loadout loadout2 = new Loadout();
+        loadout2.setArmId(12L);
+        loadout2.setLegId(14L);
+
+        when(repository.findOne(0L)).thenReturn(loadout1);
+        when(repository.saveAndFlush(loadout1)).thenReturn(loadout1);
+
+        assertEquals(loadoutController.updateLoadout(0L,loadout2).getLegId(), new Long(14));
+        assertEquals(loadoutController.updateLoadout(0L,loadout2).getArmId(), new Long(12));
 
     }
 }
