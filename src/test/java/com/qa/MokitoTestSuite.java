3626b86ba3536cfda669bd94aa510a4722e1801b
Jacob Eddison <jacob.eddison@qa.com>
2019-09-26 17:56:09 +0100

    created service tests

--- src/test/java/com/qa/MokitoTestSuite.java
+++ src/test/java/com/qa/MokitoTestSuite.java
@@ -3,12 +3,14 @@ package com.qa;
 import com.qa.controllers.ArmourControllerTest;
 import com.qa.controllers.LoadoutControllerTest;
 
+import com.qa.service.ArmourServiceTest;
+import com.qa.service.LoadoutServiceTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 
 @RunWith(Suite.class)
-@SuiteClasses({ArmourControllerTest.class, LoadoutControllerTest.class, AppTest.class})
+@SuiteClasses({ArmourControllerTest.class, ArmourServiceTest.class, LoadoutControllerTest.class, LoadoutServiceTest.class, AppTest.class})
 public class MokitoTestSuite {
 }
